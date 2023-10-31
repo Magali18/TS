096e4f21e7747a7bd77b1806e83000865752affa
Magali18 <pereyrahmagali@gmail.com>
2023-10-31 11:38:09 -0300

    add/function/handleChange

--- Learning Ts/react-app-with-typescript/ts/src/components/Form.tsx
+++ Learning Ts/react-app-with-typescript/ts/src/components/Form.tsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
 
 const Form =()=>{
+   
     const [inputValues, setInputValues]=useState({
         nick:'',
         subMoths: 0,
         avatar:'',
         description:''
+    });
+    const handleChange =(e:React.ChangeEvent<HTMLInputElement>)=>{
+    setInputValues({
+        ...inputValues,
+        [e.target.name]:e.target.value
     })
+    }
     return (
         <div>
             <form>
-                <input value={inputValues.nick} type='text' name='nick' placeholder="nick"/> 
-                <input value={inputValues.subMoths} type='text' name='subMoths' placeholder="subMoths"/> 
-                <input value={inputValues.avatar} type='text' name='avatar' placeholder="avatar"/> 
-                <input value={inputValues.description} type='text' name='description' placeholder="description"/> 
+                <input onChange={handleChange} value={inputValues.nick} type='text' name='nick' placeholder="nick"/> 
+                <input onChange={handleChange} value={inputValues.subMoths} type='text' name='subMoths' placeholder="subMoths"/> 
+                <input onChange={handleChange} value={inputValues.avatar} type='text' name='avatar' placeholder="avatar"/> 
+                <input onChange={handleChange} value={inputValues.description} type='text' name='description' placeholder="description"/> 
                 <button> Save new Subs</button>
                 </form>
 
