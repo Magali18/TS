c7f7dfa0e8d5ab0e6ab3ab0528177c6831aeb1d0
Magali18 <pereyrahmagali@gmail.com>
2023-11-01 18:14:43 -0300

    v2.5

--- Learning Ts/react-app-with-typescript/ts/src/components/Form.tsx
+++ Learning Ts/react-app-with-typescript/ts/src/components/Form.tsx
@@ -21,7 +21,7 @@ const Form =()=>{
                 <input onChange={handleChange} value={inputValues.subMoths} type='text' name='subMoths' placeholder="subMoths"/> 
                 <input onChange={handleChange} value={inputValues.avatar} type='text' name='avatar' placeholder="avatar"/> 
                 <input onChange={handleChange} value={inputValues.description} type='text' name='description' placeholder="description"/> 
-                <button> Save new Subs</button>
+                <button> Guardar </button>
                 </form>
 
         </div>
